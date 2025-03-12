fc9edddb4f17cd2c940e2f85d405f7a0fd6214c0
yuye <18838280615@163.com>
2025-03-12 17:57:29 +0800

    提交

--- src/pages/home/concert-detail.vue
+++ src/pages/home/concert-detail.vue
@@ -385,7 +385,7 @@ const handleSubmit = async () => {
   } else {
     uniShowToast('已登录');
     // 订阅微信订阅
-    SubscribeWXTemplateAPI('XBSM', '/api/v1/wx/mini.send.subscribe.message');
+    SubscribeWXTemplateAPI('XBSM', '/api/v1/wx/mini.save.subscribe.template');
     return;
   }
   const perform_skuList_select = perform_skuList.value.filter((item: any) => {
